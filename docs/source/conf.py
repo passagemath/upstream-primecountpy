ec41d2b591501d116ab93dd2340600e3e568e478
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2025-07-02 23:08:44 -0700

    Version 0.1.1.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, SageMath Developers'
 author = 'SageMath Developers'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.1.1'
 
 
 # -- General configuration ---------------------------------------------------
